14c0ba67bfe6e6adee831b4f183cbeabad375d96
Nguyen Pham <nguyenpv@gem-corp.tech>
2026-04-02 23:28:56 +0700

    test: add age to user fixtures

--- tests/fixtures/user.fixture.js
+++ tests/fixtures/user.fixture.js
@@ -14,6 +14,7 @@ const userOne = {
   password,
   role: 'user',
   isEmailVerified: false,
+  age: 25,
 };
 
 const userTwo = {
@@ -23,6 +24,7 @@ const userTwo = {
   password,
   role: 'user',
   isEmailVerified: false,
+  age: 30,
 };
 
 const admin = {
@@ -32,6 +34,7 @@ const admin = {
   password,
   role: 'admin',
   isEmailVerified: false,
+  age: 35,
 };
 
 const insertUsers = async (users) => {
